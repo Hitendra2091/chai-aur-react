78266428ee7cd3fa6267f2fff8740e9ace9351f1
Hitendra2091 <hitendrasdeshmukh@gmail.com>
2026-05-07 15:27:29 +0530

    Interview

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+      <h1 className='bg-green-400 text-white p-4 rounded-xl'>Tailwind test</h1>
+      <Card username="Hitendra"/>
+      <div className='mt-6'>
+      <Card username="Deshmukh" btntext="Click Me" />
+      </div>
+    </>
+  )
+}
+
+export default App
